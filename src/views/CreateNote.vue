2a2f7b79403f3237e1191284c367e215dc77c789
Gabriel Heinrich <gabriel@codecampleipzig.de>
2021-05-06 11:31:09 +0200

    Setup v-model

--- src/views/CreateNote.vue
+++ src/views/CreateNote.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1>Take a note</h1>
-    <form>
+    <form @submit.prevent="submit">
       <div>
         <label for="title" class="sr-only">Title</label>
-        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="title" placeholder="Title" v-model="title" />
       </div>
       <div>
         <label for="content" class="sr-only">Content</label>
-        <textarea placeholder="Content" />
+        <textarea placeholder="Content" v-model="content" />
       </div>
       <div>
         <button type="submit">Create</button>
@@ -18,7 +18,19 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      title: "",
+      content: "",
+    };
+  },
+  methods: {
+    async submit() {
+      console.log("Title: ", this.title, "Content: ", this.content);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
