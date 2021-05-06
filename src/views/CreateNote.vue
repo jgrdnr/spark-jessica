5734f14fc58eebde0b1097da5eba58c9a5361b07
Gabriel Heinrich <gabriel@codecampleipzig.de>
2021-05-06 11:26:44 +0200

    Setup create form

--- src/views/CreateNote.vue
+++ src/views/CreateNote.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <h1>Take a Note</h1>
+    <h1>Take a note</h1>
+    <form>
+      <div>
+        <label for="title" class="sr-only">Title</label>
+        <input type="text" name="title" placeholder="Title" />
+      </div>
+      <div>
+        <label for="content" class="sr-only">Content</label>
+        <textarea placeholder="Content" />
+      </div>
+      <div>
+        <button type="submit">Create</button>
+      </div>
+    </form>
   </div>
 </template>
 
